5b4b5f69834a7b24504317aef8087eef2dbd647d
Nick Harberg <nick@seamonsterstudios.com>
2014-07-25 14:17:30 -0700

    purposed changes

---
+++ header.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en-US">
+<meta http-equiv="content-type" content="text/html;charset=UTF-8" />
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=yes">
+  <title><?php the_title(); ?></title>
+  <link rel="shortcut icon" type="image/png" href=".png" />
+  <?php wp_head(); ?>
+  <link rel="stylesheet" type='text/css' href="<?php bloginfo('stylesheet_url'); ?>">
+</head>
+
+<body class="home blog group-blog">
+  <div id="page" class="hfeed site">
+    <header id="masthead" class="site-header" role="banner">
+      <div class="site-branding clear">
+        <h1 class="site-title">
+          <a href="http://www.zenpurchase.com/" class="logo-zenpurchase">ZenPurchase Home</a>
+          <a href="index.html" title="ZenPurchase" rel="home" class="logo-community">ZenPurchase</a>
+        </h1>
+        <div class="site-buttons pull-right no-mobile">
+          <a href="http://www.zenpurchase.com/signup" target="_blank">Sign Up</a>
+          <a href="https://.com/" target="_blank" class="btn">Place Holder</a>
+        </div>
+      </div>
+
+      <nav id="site-navigation" class="navigation-main" role="navigation">
+        <h1 class="menu-toggle">Menu</h1>
+        <div class="screen-reader-text skip-link"><a href="#content" title="Skip to content">Skip to content</a></div>
+
+        <div class="menu-community-container"><ul id="menu-community" class="menu"><li id="menu-item-231" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-231"><a href="http://www.zenpurchase.com/">ZenPurchase Home</a></li>
+          <li id="menu-item-110" class="menu-item menu-item-type-custom menu-item-object-custom current-menu-item current_page_item menu-item-home menu-item-110"><a href="index.html">Blog</a></li>
+          <li id="menu-item-235" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-235"><a href="archives/index.html">Archives</a></li>
+          <li id="menu-item-50" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-50"><a href="events/index.html">Events</a></li>
+        </ul></div>   </nav><!-- #site-navigation -->
+      </header><!-- #masthead -->
